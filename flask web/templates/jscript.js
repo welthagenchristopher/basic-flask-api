08d02a90ae4b61d33fbfd15aeeed3f19c0271516
welthagenchristopher <welthagen.christopher@gmail.com>
2024-11-18 09:31:33 +1300

    Add files via upload

---
+++ flask web/templates/jscript.js
@@ -0,0 +1,25 @@
+document.getElementById('payment-form').addEventListener('submit', function(event) {
+
+    event.preventDefault();
+    const messageElement = document.getElementById('message');
+
+    // Simulation of payment capture. Do NOT use this, it's so bad - use a 3rd party service instead.
+    const name = document.getElementById('card-name').value;
+    const cardNumber = document.getElementById('card-number').value;
+    const expiryDate = document.getElementById('expiry-date').value;
+    const cvv = document.getElementById('cvv').value;
+
+
+    if (name && cardNumber && expiryDate && cvv) {
+    
+        messageElement.textContent = 'Payment processed successfully! Thank you for your purchase.';
+        messageElement.style.color = 'green';
+        
+        document.getElementById('payment-form').reset();
+
+    } else {
+        messageElement.textContent = 'Please fill in all fields.';
+        messageElement.style.color = 'red';
+    }
+    
+});
